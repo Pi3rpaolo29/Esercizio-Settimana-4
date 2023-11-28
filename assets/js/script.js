37755c3cbe43df7bbe241a881b2aafa2a66bf065
Pierpaolo Giorgi <pierpaologiorgiiccs@gmail.com>
2023-11-28 14:47:47 +0100

    Esercizio Settimana 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 document.addEventListener('scroll', () => {
     const navbar = document.querySelector('header');
+    const button = document.querySelector('.navbar ul li:nth-child(5) a')
     if(window.scrollY > 350){
         navbar.classList.add('scrolled');
+        button.classList.add('scrolled');
     }
     else{
         navbar.classList.remove('scrolled');
+        button.classList.remove('scrolled');
     }
 })
 
